2d9414c123bb1a82463dd93d46551a160439587d
denys19973 <oxingteam@gmail.com>
2020-09-26 23:10:44 +0000

    added missed files for: • added visualization saving on each step • small optimization and progress indicating in image preprocessing • clean up of commented code and comments

--- train_AEI.py
+++ train_AEI.py
@@ -1,8 +1,10 @@
 from network.AEI_Net import *
 from network.MultiscaleDiscriminator import *
 from utils.Dataset import FaceEmbed, With_Identity
+from utils.visual_check import VisualChecker
 from torch.utils.data import DataLoader
 import torch.optim as optim
+import torchvision.transforms as transforms
 from face_modules.model import Backbone, Arcface, MobileFaceNet, Am_softmax, l2_norm
 import torch.nn.functional as F
 import torch
@@ -11,22 +13,25 @@ import torchvision
 import cv2
 from apex import amp
 import visdom
+from utils.visdom_plotter import VisdomLinePlotter
 
+vis = visdom.Visdom(env='faceshifter', port=8097)
+plotter = VisdomLinePlotter(vis, env_name='AEI Training')
 
-vis = visdom.Visdom(server='127.0.0.1', env='faceshifter', port=8099)
-batch_size = 16
+batch_size = 6
 lr_G = 4e-4
 lr_D = 4e-4
 max_epoch = 2000
-show_step = 10
+show_step = 1
+save_visual_check_epoch = 1
 save_epoch = 1
 model_save_path = './saved_models/'
 optim_level = 'O1'
 
-# fine_tune_with_identity = False
+VISUAL_CHECK_SOURCE_PATH = '../aligned_my/00000001.jpg'
+VISUAL_CHECK_TARGET_PATH = '../aligned_my/00000000.jpg'
 
 device = torch.device('cuda')
-# torch.set_num_threads(12)
 
 G = AEI_Net(c_id=512).to(device)
 D = MultiscaleDiscriminator(input_nc=3, n_layers=6, norm_layer=torch.nn.InstanceNorm2d).to(device)
@@ -35,7 +40,7 @@ D.train()
 
 arcface = Backbone(50, 0.6, 'ir_se').to(device)
 arcface.eval()
-arcface.load_state_dict(torch.load('./face_modules/model_ir_se50.pth', map_location=device), strict=False)
+arcface.load_state_dict(torch.load('./saved_models/model_ir_se50.pth', map_location=device), strict=False)
 
 opt_G = optim.Adam(G.parameters(), lr=lr_G, betas=(0, 0.999))
 opt_D = optim.Adam(D.parameters(), lr=lr_D, betas=(0, 0.999))
@@ -49,14 +54,9 @@ try:
 except Exception as e:
     print(e)
 
-# if not fine_tune_with_identity:
-    # dataset = FaceEmbed(['../celeb-aligned-256_0.85/', '../ffhq_256_0.85/', '../vgg_256_0.85/', '../stars_256_0.85/'], same_prob=0.5)
-# else:
-    # dataset = With_Identity('../washed_img/', 0.8)
-dataset = FaceEmbed(['../celeb-aligned-256_0.85/', '../ffhq_256_0.85/', '../vgg_256_0.85/', '../stars_256_0.85/'], same_prob=0.8)
+dataset = FaceEmbed(['../celeba_64/'], same_prob=0.8)
 
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True)
-
 
 MSE = torch.nn.MSELoss()
 L1 = torch.nn.L1Loss()
@@ -64,9 +64,9 @@ L1 = torch.nn.L1Loss()
 
 def hinge_loss(X, positive=True):
     if positive:
-        return torch.relu(1-X).mean()
+        return torch.relu(1 - X).mean()
     else:
-        return torch.relu(X+1).mean()
+        return torch.relu(X + 1).mean()
 
 
 def get_grid_image(X):
@@ -82,10 +82,11 @@ def make_image(Xs, Xt, Y):
     return torch.cat((Xs, Xt, Y), dim=1).numpy()
 
 
-# prior = torch.FloatTensor(cv2.imread('./prior.png', 0).astype(np.float)/255).to(device)
+visual_checker = VisualChecker(VISUAL_CHECK_SOURCE_PATH, VISUAL_CHECK_TARGET_PATH, embedder=arcface)
+
 
 print(torch.backends.cudnn.benchmark)
-#torch.backends.cudnn.benchmark = True
+# torch.backends.cudnn.benchmark = True
 for epoch in range(0, max_epoch):
     # torch.cuda.empty_cache()
     for iteration, data in enumerate(dataloader):
@@ -95,9 +96,10 @@ for epoch in range(0, max_epoch):
         Xt = Xt.to(device)
         # embed = embed.to(device)
         with torch.no_grad():
-            embed, Xs_feats = arcface(F.interpolate(Xs[:, :, 19:237, 19:237], [112, 112], mode='bilinear', align_corners=True))
+            embed, Xs_feats = arcface(
+                F.interpolate(Xs[:, :, 19:237, 19:237], [112, 112], mode='bilinear', align_corners=True))
         same_person = same_person.to(device)
-        #diff_person = (1 - same_person)
+        # diff_person = (1 - same_person)
 
         # train G
         opt_G.zero_grad()
@@ -108,32 +110,30 @@ for epoch in range(0, max_epoch):
 
         for di in Di:
             L_adv += hinge_loss(di[0], True)
-        
 
         Y_aligned = Y[:, :, 19:237, 19:237]
         ZY, Y_feats = arcface(F.interpolate(Y_aligned, [112, 112], mode='bilinear', align_corners=True))
-        L_id =(1 - torch.cosine_similarity(embed, ZY, dim=1)).mean()
+        L_id = (1 - torch.cosine_similarity(embed, ZY, dim=1)).mean()
 
         Y_attr = G.get_attr(Y)
         L_attr = 0
         for i in range(len(Xt_attr)):
             L_attr += torch.mean(torch.pow(Xt_attr[i] - Y_attr[i], 2).reshape(batch_size, -1), dim=1).mean()
+
         L_attr /= 2.0
 
-        L_rec = torch.sum(0.5 * torch.mean(torch.pow(Y - Xt, 2).reshape(batch_size, -1), dim=1) * same_person) / (same_person.sum() + 1e-6)
+        L_rec = torch.sum(0.5 * torch.mean(torch.pow(Y - Xt, 2).reshape(batch_size, -1), dim=1) * same_person) / (
+                same_person.sum() + 1e-6)
 
-        lossG = 1*L_adv + 10*L_attr + 5*L_id + 10*L_rec
-        # lossG = 1*L_adv + 10*L_attr + 5*L_id + 10*L_rec
+        lossG = 1 * L_adv + 10 * L_attr + 5 * L_id + 10 * L_rec
+
         with amp.scale_loss(lossG, opt_G) as scaled_loss:
             scaled_loss.backward()
 
-        # lossG.backward()
         opt_G.step()
 
         # train D
         opt_D.zero_grad()
-        # with torch.no_grad():
-        #     Y, _ = G(Xt, embed)
         fake_D = D(Y.detach())
         loss_fake = 0
         for di in fake_D:
@@ -143,24 +143,28 @@ for epoch in range(0, max_epoch):
         loss_true = 0
         for di in true_D:
             loss_true += hinge_loss(di[0], True)
-        # true_score2 = D(Xt)[-1][0]
 
-        lossD = 0.5*(loss_true.mean() + loss_fake.mean())
+        lossD = 0.5 * (loss_true.mean() + loss_fake.mean())
 
         with amp.scale_loss(lossD, opt_D) as scaled_loss:
             scaled_loss.backward()
-        # lossD.backward()
         opt_D.step()
         batch_time = time.time() - start_time
         if iteration % show_step == 0:
             image = make_image(Xs, Xt, Y)
             vis.image(image[::-1, :, :], opts={'title': 'result'}, win='result')
-            cv2.imwrite('./gen_images/latest.jpg', image.transpose([1,2,0]))
+            cv2.imwrite('./gen_images/latest.jpg', image.transpose([1, 2, 0]))
         print(f'epoch: {epoch}    {iteration} / {len(dataloader)}')
-        print(f'lossD: {lossD.item()}    lossG: {lossG.item()} batch_time: {batch_time}s')
-        print(f'L_adv: {L_adv.item()} L_id: {L_id.item()} L_attr: {L_attr.item()} L_rec: {L_rec.item()}')
+        if iteration % 100 == 0:
+            plotter.plot('loss G, D', 'D', 'Discriminator loss', iteration, lossD.item())
+            plotter.plot('loss G, D', 'G', 'Generator loss', iteration, lossG.item())
+            plotter.plot('loss adv, id, attr', 'adv', 'Adversarial loss', iteration, L_adv.item())
+            plotter.plot('loss adv, id, attr', 'id', 'Identity loss', iteration, L_id.item())
+            plotter.plot('loss adv, id, attr', 'attr', 'Attributes loss', iteration, L_attr.item())
+            print(f'lossD: {lossD.item()}    lossG: {lossG.item()} batch_time: {batch_time}s')
+            print(f'L_adv: {L_adv.item()} L_id: {L_id.item()} L_attr: {L_attr.item()} L_rec: {L_rec.item()}')
         if iteration % 1000 == 0:
             torch.save(G.state_dict(), './saved_models/G_latest.pth')
             torch.save(D.state_dict(), './saved_models/D_latest.pth')
-
-
+    if epoch % save_visual_check_epoch == 0:
+        visual_checker.save_visual_result(G, epoch)
